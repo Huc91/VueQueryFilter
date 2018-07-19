6872a12ff2f1ae71d7f240c69c643f146911c3c5
Huc <luca.ucciero@gmail.com>
2018-07-19 14:52:47 +0200

    added plugin

---
+++ VueQueryFilter/index.js
@@ -0,0 +1,18 @@
+import * as QueryQl from '@/../node_modules/queryfilters/dist/queryql-min.js'
+
+const VueQueryFilter = {
+	install( Vue ) {
+
+		const queryql = new QueryQl()
+
+    Vue.prototype.$queryql = queryql;
+    Vue.queryql = queryql;
+
+	},
+}
+
+export default VueQueryFilter
+
+// Automatic installation if Vue has been added to the global scope.
+if( typeof window !== 'undefined' && window.Vue && window.rollbarConfig )
+	window.Vue.use( VueQueryFilter )
